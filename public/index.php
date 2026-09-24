f7e4d1b5019227a6ccdb70693a8415005923047b
Juan Martinez <juanmartinezcastillo9sena@gmail.com>
2026-09-24 17:06:24 -0500

    Configuracion de enrutamiento inicial con rewrite y get

--- public/index.php
+++ public/index.php
@@ -1,8 +1,17 @@
-<?php 
+<?php
 
 require_once __DIR__ . "/../app/controllers/apartamentoController.php";
 
-$ApartamentoController = new apartamentoController();
-$ApartamentoController->index();
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
 
+?>
+
+<a href="/apartamento">Apartamentos</a>
+
+<?php
+if ($method === 'GET' && $uri === '/apartamento') {
+    $ApartamentoController = new apartamentoController();
+    $ApartamentoController->index();
+}
 ?>
