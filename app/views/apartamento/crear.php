2e16a23ad47d9b667ce50b7114b1502f78ffe3a1
Juan Martinez <juanmartinezcastillo9sena@gmail.com>
2026-09-24 17:38:45 -0500

    Formulario

---
+++ app/views/apartamento/crear.php
@@ -0,0 +1,12 @@
+<form action"/apartamento" method="POST">
+    <input type="text" name="nombre">
+    <input type="text" name="tipovivienda">
+    <input type="text" name="estado">
+    <input type="text" name="direccion">
+    <input type="number" name="area">
+    <input type="number" name="habitaciones">
+    <input type="number" name="banos">
+    <input type="number" name="parqueadero">
+    <input type="number" name="valor">
+    <button type="submit">Guardar</button>
+</form>
